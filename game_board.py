c5f835c67dfbf44e04f7bd4de84fa15de0c8a732
0lyaOrlova <orlova.olga.d@yandex.ru>
2025-01-22 15:18:33 +0300

    Прописана повторная генерация лазера

--- game_board.py
+++ game_board.py
@@ -16,7 +16,7 @@ FPS = 60
 
 
 x = 513
-y = randrange(5, 400)
+n = randrange(5, 400)
 c = 5
 
 bg = pygame.image.load('data/поле.png').convert()
@@ -43,8 +43,8 @@ a = []
 if __name__ == '__main__':
     running = True
     screen.blit(bg, (0, 0))
-    pygame.draw.circle(screen, (0, 255, 0), (1, y), 5)
-    pygame.draw.circle(screen, (255, 255, 255), (1, y), 2)
+    pygame.draw.circle(screen, (0, 255, 0), (1, n), 5)
+    pygame.draw.circle(screen, (255, 255, 255), (1, n), 2)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -56,32 +56,34 @@ if __name__ == '__main__':
                         mirror.rect.x = pos[0]
         screen.blit(bg, (0, 0))
         missle.rect.y += speed
-        if (missle.rect.x - 20) < c and  (missle.rect.x + 20) > c and (missle.rect.y - 20) < y and (missle.rect.y + 20) > y:
+        if (missle.rect.x - 20) < abs(c) and  (missle.rect.x + 20) > abs(c) and (missle.rect.y - 20) < n and (missle.rect.y + 20) > n:
             print("winner") # Показатель того, что снаряд поражен, в дальнейшем здесь будет анимация поражения снаряда
-        if y >= 715:
+        if n >= 716:
+            n = randrange(10, 350)
             c = 5
-            y = randrange(5, 400)
-        if (mirror.rect.x - 30) < c and  (mirror.rect.x + 30) > c and (mirror.rect.y - 20) < y and (mirror.rect.y + 20) > y:
+            pygame.draw.circle(screen, (0, 255, 0), (25, n), 5)
+            pygame.draw.circle(screen, (255, 255, 255), (25, n), 2)
+        if (mirror.rect.x - 30) < abs(c) and  (mirror.rect.x + 30) > abs(c) and (mirror.rect.y - 20) < n and (mirror.rect.y + 20) > n:
             a.append(1)
-        if y <= 5:
+        if n <= 5:
             a.append(2)
         if c >= 970:
             a.append(0)
         if len(a) > 0:
             if a[-1] == 1:
                 c += 3
-                y -= 3
+                n -= 3
             elif a[-1] == 0:
                 c -= 3
-                y += 3
+                n += 3
             elif a[-1] == 2:
                 c += 3
-                y += 3
+                n += 3
         else:
-            y += 3
+            n += 3
             c += 3
-        pygame.draw.circle(screen, (0, 255, 0), (c, y), 5)
-        pygame.draw.circle(screen, (255, 255, 255), (c, y), 2)
+        pygame.draw.circle(screen, (0, 255, 0), (abs(c), n), 5)
+        pygame.draw.circle(screen, (255, 255, 255), (abs(c), n), 2)
         screen.blit(mirror.image, mirror.rect)
         screen.blit(missle.image, missle.rect)
         pygame.display.update()
